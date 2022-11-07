0bf3955fdbc00418aab2983ac49c8480a4bb178f
Rihanshu <rihanshur@gmail.com>
2022-11-07 13:50:28 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
+var items = ["Buy Food","Cook Food","Eat Food"]
+
+app.get("/", function(req, res) {
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day : "numeric",
+    month: "long"
+  }
+  var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {
+      findday: day,
+      newlistItem: items
+    })
+})
+
+app.post("/",function(req,res){
+  var item = req.body.newItem;
+  items.push(item)
+  res.redirect("/")
+})
+
+app.listen(3000, function() {
+  console.log("Server Running")
+})
