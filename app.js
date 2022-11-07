0d807b9bc8a9494154a6ff463275f320ebec38d8
Rihanshur <88155603+Rihanshur@users.noreply.github.com>
2022-11-07 14:58:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.post("/",function(req,res){
   res.redirect("/")
 })
 
-app.listen(https://rihanshur.github.io/To-Do-list/, function() {
+app.listen("https://rihanshur.github.io/To-Do-list/", function() {
   console.log("Server Running")
 })
